c105e3e7ee282de8bd998e3fa66e2bd79c9833f7
anjali07 <asen53184@gmail.com>
2021-07-23 20:30:42 +0530

    select with where and condition(OR)

--- selectQuery.php
+++ selectQuery.php
@@ -1,7 +1,7 @@
 <?php
  require_once("connection.php");
  require_once("header.php");
-   $selectQuery= "Select *  from sqlPractice where ID = 1";
+   $selectQuery= "Select * from sqlPractice where city = 'gwalior' or age='30'";
      $result=mysqli_query($conn,$selectQuery);
       if(mysqli_num_rows($result)>0){
             ?>
