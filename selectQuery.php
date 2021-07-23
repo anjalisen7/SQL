296ff067176b51744c4f84573fd2cf1d29678b51
anjali07 <asen53184@gmail.com>
2021-07-23 18:20:41 +0530

    select query to fetch all data

---
+++ selectQuery.php
@@ -0,0 +1,47 @@
+<?php
+ require_once("connection.php");
+ require_once("header.php");
+   $selectQuery= "Select * from sqlPractice";
+     $result=mysqli_query($conn,$selectQuery);
+      if(mysqli_num_rows($result)>0){
+            ?>
+            <body background="grey">
+            <div class="container">
+                        <table class="table-stripped table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>S.NO.</th>
+                                    <th>Name</th>
+                                    <th>Age</th>
+                                    <th>EMAIL</th>
+                                    <th>City</th>
+                                </tr>
+                            </thead>
+                            <?php
+                     while($row = mysqli_fetch_assoc($result)){?>
+                            <tbody>
+                                <tr>
+                                    <td>*</td>
+                                    <td><?php print_r($row['name']);?></td>
+                                    <td><?php print_r($row['age']);?></</td>
+                                    <td><?php print_r($row['email']);?></</td>
+                                    <td><?php print_r($row['city']);?></</td>
+                                </tr>
+                            </tbody>
+                                <?php }?>
+                        </table>
+             </div> 
+            
+        </body>  
+        <?php  
+      }else{
+          echo"error";
+      }
+
+?>
+
+
+
+
+
+
