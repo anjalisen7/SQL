b479652c1d985fe1655daf4097cbd788cd7b2b24
anjali07 <asen53184@gmail.com>
2021-07-28 12:18:55 +0530

    insertQuery

---
+++ formInsert.php
@@ -0,0 +1,18 @@
+<div class="col-lg-6 m-auto" id="contain">
+        <form action="" method="post">
+        <div class="card" id="dine">
+            <div class="card-header bg-dark">
+            <h2 class="text-white text-center">INSERT DATA</h2>
+            </div>
+        </div>
+            <label>Username</label>
+              <input type="text" name="name" class="form-control" required><br><br>
+           <label>age</label>
+             <input type="number" name="age" class="form-control" required><br><br>
+             <label>Email</label>
+            <input type="email" name="mail" class="form-control" required><br><br>
+            <label>city</label>
+             <input type="text" name="city" class="form-control" required><br><br>
+           <button type="submit" class="btn btn-secondary" name="click">Submit</button>
+        </form>
+    </div>
---
+++ insert.php
@@ -0,0 +1,17 @@
+<?Php
+require_once("connection.php");
+require_once("header.php");
+ if(isset($_POST['click'])){
+    $name=$_POST['name'];
+    $email=$_POST['mail'];
+    $age=$_POST['age'];
+    $city=$_POST['city'];
+    $insertQuery= "INSERT into `sqlpractice`(`name`,`email`,`age`,`city`)Values('$name','$email','$age','$city')";
+    $result=mysqli_query($conn,$insertQuery);
+ }
+ ?>
+ <body>
+     <?php
+     require_once("formInsert.php");
+     ?>
+ </body>
