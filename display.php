fac0ba255163a08bff95df9e281dd876cbc4ecd2
anjali07 <asen53184@gmail.com>
2021-07-30 14:08:09 +0530

    MIN(with multiple coloum)

--- display.php
+++ display.php
@@ -8,27 +8,29 @@
   
                     <table class="table  text-center table-stripped table-hover table-bordered">
                         <tr class="table table-success table-stripped">
-                            <th>ID</th>
+                            <!-- <th>ID</th> -->
                             <th>Name</th>
                             <th>Age</th>
-                            <th>Email</th>
+                            <!-- <th>Email</th> -->
                             <th>City</th>
+                            <th>salary</th>
                             <th>Edit</th>
                             <th>Delete</th>
                         </tr>
 
                      <?php
                       require_once("connection.php");
-                     $selectQuery= "SELECT  * FROM sqlpractice LIMIT 4";
+                      $selectQuery= "SELECT MIN(salary) as salary,name,city,age FROM sqlpractice";
                      $query = mysqli_query($conn,$selectQuery);
                       while($result=mysqli_fetch_array($query)){
+                   
                      ?>
                         <tr>
-                            <td><?php echo $result['ID']?></td>
+                           
                             <td><?php echo $result['name']?></td>
-                            <td><?php echo $result['age']?></td>
-                            <td><?php echo $result['email']?></td>
+                            <td><?php echo $result['age']?></td>                     
                             <td><?php echo $result['city']?></td>
+                            <td><?php echo  $result['salary'];?></td>
                             <td><button class="btn btn-secondary ">Edit</button></td>
                             <td>Delete</td>
                        </tr> 
@@ -39,7 +41,7 @@
 
                 </div>
             </div>
-
+            <a href="insert.php"><button type="button" name="back" class="btn btn-success btn-lg"> 🔙 Back</button></a>
             </body>
             </html>
 
