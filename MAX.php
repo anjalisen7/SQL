9bc34613ec32644ff6fa2f6de35f3d5737a07e59
anjali07 <asen53184@gmail.com>
2021-07-30 13:54:49 +0530

    max()return maximum value

---
+++ MAX.php
@@ -0,0 +1,11 @@
+<?php
+
+ require_once("header.php");
+ require_once("connection.php");
+ $selectQuery= "SELECT MAX(salary) as salary FROM sqlpractice";
+ $query = mysqli_query($conn,$selectQuery);
+  $values = mysqli_fetch_assoc($query);
+  $result = $values['salary'];
+   echo "MAX-salary =>";
+  echo $result;
+?>
