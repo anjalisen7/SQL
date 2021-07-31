5f9dbae66327b714d20376c9cb7cf452b2cc66f4
anjali07 <asen53184@gmail.com>
2021-07-31 12:14:01 +0530

    select selected column on the basis of selected common id

--- display.php
+++ display.php
@@ -9,33 +9,33 @@
                     <table class="table  text-center table-stripped table-hover table-bordered">
                         <tr class="table table-success table-stripped">
                             <th>ID</th>
-                            <th>Name</th>
-                            <th>Age</th>
-                            <th>City</th>
+                            <!-- <th>Name</th> -->
+                            <!-- <th>Age</th> -->
+                            <!-- <th>City</th> -->
                             <th>Email</th>
-                            <th>salary</th>
+                            <!-- <th>salary</th> -->
                             <th>POST</th>
-                            <th>phone-no.</th>
+                            <!-- <th>phone-no.</th> -->
                             <th>Edit</th>
                             <th>Delete</th>
                         </tr>
 
                      <?php
                       require_once("connection.php");
-                      $selectQuery= "SELECT * FROM sqlpractice INNER JOIN employee ON sqlpractice.ID=employee.employee_id";
+                      $selectQuery= "SELECT sqlpractice.ID, sqlpractice.email, employee.Post FROM sqlpractice INNER JOIN employee ON sqlpractice.ID=employee.employee_id";
                       $query = mysqli_query($conn,$selectQuery);
                       while($result= mysqli_fetch_array($query)){
                                
                      ?>
                         <tr>
                         <td><?php echo $result['ID']?></td>
-                        <td><?php echo $result['name']?></td>
+                        <!-- <td><?php echo $result['name']?></td>
                         <td><?php echo $result['age']?></td>                     
-                        <td><?php echo $result['city']?></td>
+                        <td><?php echo $result['city']?></td> -->
                         <td><?php echo $result['email']?></td>
-                        <td><?php echo $result['salary'];?></td>
+                        <!-- <td><?php echo $result['salary'];?></td> -->
                         <td><?php echo $result['Post'];?></td>
-                        <td><?php echo $result['phone-no.'];?></td>
+                        <!-- <td><?php echo $result['phone-no.'];?></td> -->
                             <td><button class="btn btn-secondary ">Edit</button></td>
                             <td>Delete</td>
                        </tr> 
